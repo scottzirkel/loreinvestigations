98cf50f4f1b2f1dc3a48a967c0ebcd244c6fd849
Scott Zirkel <scott@scottzirkel.com>
2025-08-18 16:44:02 -0500

    Included image in remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/743c857524a2174eba3c2a3cc69bb696/**",
+      ),
+    ],
+  },
+};
 
 export default nextConfig;
