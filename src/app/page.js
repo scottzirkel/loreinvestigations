d43a67c1b0343299c79433de0c72368f08741b9f
Scott Zirkel <scott@scottzirkel.com>
2025-08-18 16:21:16 -0500

    moving to nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,172 @@
-import Image from 'next/image'
-
-async function getData(slug) {
-  let header = new Headers({
-    Authorization: `Bearer ${process.env.WEBSTRIPS_API_TOKEN}`,
-    'Content-Type': 'application/json',
-  })
-  const res = await fetch(`${process.env.WEBSTRIPS_API_URL}/strips`, {
-    cache: 'no-store',
-    headers: header,
-  })
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
-}
-
-export default async function Page() {
-  const data = await getData()
-  const images = Object.values(data.images)
-
+export default function Home() {
   return (
-    <main>
-      <div className="mt-8">
-        {images.map((image, index) => (
-          <Image key={image.uuid} src={image.original_url} width={1200} height={1200} alt={data.title} loading={index > 0 ? 'lazy' : 'eager'} />
-        ))}
+    <main className="mx-auto px-4 text-center md:px-12 lg:max-w-7xl">
+      <div className="mx-auto max-w-2xl space-y-2 p-4 text-lg text-stone-200">
+        <p>
+          Lore Investigations follows the paranormal investigative team of
+          Nathan and TJ. Their first case is The Creepy Barn, in which they go
+          up against what is possibly a possessed teenage girl. Of course, it's
+          hard to tell a regular teenage girl from one possessed by a ghost.
+        </p>
+        <p>
+          Lore Investigations is written by Amanda Jones and
+          <a
+            href="https://scottzirkel.com"
+            target="_blank"
+            className="transition hover:text-red-700"
+          >
+            Scott Zirkel
+          </a>
+          , with art by
+          <a
+            href="http://www.matthebb.com"
+            target="_blank"
+            className="text-stone-400 underline transition hover:text-red-700"
+          >
+            Matt Hebb
+          </a>
+          .&nbsp;Jason Worthington is on inks and Juan Carlos Porras bringing up
+          the colors.
+        </p>
       </div>
+      <footer className="space-y-2 text-center text-sm text-stone-400">
+        <p>
+          Copyright © 2009-{new Date().getFullYear()}
+          &nbsp;
+          <a
+            className="text-red-700 transition hover:text-red-500"
+            href="https://scottzirkel.com"
+            target="_blank"
+          >
+            Scott Zirkel.
+          </a>
+          &nbsp;All rights reserved.
+        </p>
+        <p>
+          Powered by &nbsp;
+          <a
+            className="text-red-700 transition hover:text-red-500"
+            href="https://webstrips.app"
+            target="_blank"
+          >
+            Webstrips
+          </a>
+        </p>
+      </footer>
+      <section className="mt-8">
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/743c857524a2174eba3c2a3cc69bb696/cover.jpg"
+          alt="cover"
+          loading="eager"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/156696c4f1e82ae95a8cfe166edee254/Page 1.jpg"
+          alt="Page 1"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/769230c97cf2be89ef583a0bf28d80d2/Page 2.jpg"
+          alt="Page 2"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/b7db56cc42bc98991b8652dea2d64d7e/Page 3.jpg"
+          alt="Page 3"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/a47c4d0713f66eac8b7bdb6617054dba/Page 4.jpg"
+          alt="Page 4"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/8a28e5df90758aa760d444961a5f7834/Page 5.jpg"
+          alt="Page 5"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/d3a6fa64bb0502d3976ed3fc553d16f0/Page 6.jpg"
+          alt="Page 6"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/e5f9c8877747d702d27100cd83cd2d4b/Page 7.jpg"
+          alt="Page 7"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/8f495e48608023a43d92d590cfcc5014/Page 8.jpg"
+          alt="Page 8"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/59717acc0aad2a376abf2352466a3a8b/Page 9.jpg"
+          alt="Page 9"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/73aad9421ba41b5ea54e6e3806de3657/Page 10.jpg"
+          alt="Page 10"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/30ce518a38b836f61451872b519f1c62/Page 11.jpg"
+          alt="Page 11"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/3d1967b35041e9b26aa0ab9c85fc25ab/Page 12.jpg"
+          alt="Page 12"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/43010f94c9fa73c90180fc48a92cc69d/Page 13.jpg"
+          alt="Page 13"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/41bfa0cafeab67390e556df5ce77275a/Page 14.jpg"
+          alt="Page 14"
+          loading="lazy"
+        />
+        <img
+          width="1184"
+          height="1831"
+          src="https://cdn.scottzirkel.com/prod/comics/com_Z3caUdQrqoN1HlfMsJ7rQ/images/4ba6a49d19a002b3dc5d2d49350b759c/Page 15.jpg"
+          alt="Page 15"
+          loading="lazy"
+        />
+      </section>
     </main>
-  )
+  );
 }
